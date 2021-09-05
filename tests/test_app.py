8a352cc9948a407b4457d817a7f4ccad31c29d39
Jarades Monteiro <jaradesm@gmail.com>
2021-09-05 19:39:58 -0300

    Improving tests, renaming app.py -> main.py

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.fixture
+def app():
+    import main
+    main.app.testing = True
+    return main.app.test_client()
+
+
+def test_index(app):
+    r = app.get('/vegetation-cover')
+    assert r.status_code == 200
