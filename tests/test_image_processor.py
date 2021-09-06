25c7ec79af97f6bd626c87ffe25603193f48094e
Jarades Monteiro <jaradesm@gmail.com>
2021-09-05 21:42:40 -0300

    fixing unit test

--- tests/test_image_processor.py
+++ tests/test_image_processor.py
@@ -2,7 +2,7 @@ from services import image_processor
 
 def test_get_local_datetime_from_filename_success():
     file_name = '319567_2331703_2016-12-07_0c0b-20161207T151953Z.tif'
-    expected = '2016-12-07T15:19:53-02:00'
+    expected = '2016-12-07T13:19:53-02:00'
     result = image_processor.get_local_datetime_from_filename(file_name, -47.597228921551284, -15.858576386589299)
     assert result == expected
 
